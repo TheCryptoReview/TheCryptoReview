8b5e73957f71fd21afaca34dd189130fb6f3102d
Theo <theomhalpern@gmail.com>
2021-12-31 18:41:41 +0000

    maybe fixed the file locations

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def home():
   return render_template("index.html")
 
-@app.route("/about")
+@app.route("/dashboard.html")
 def about():
-  return render_template("about.html")
+  return render_template("dashboard.html")
     
 app.run(host='0.0.0.0', port=8080)
