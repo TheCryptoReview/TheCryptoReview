5f900670f415824ec966d9480b94a97898ac530d
Theo <theomhalpern@gmail.com>
2021-12-31 19:05:35 +0000

    made dashboard render a little bit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index.html")
+# @app.route("/index.html")
 def home():
-  return render_template("index.html")
-
-@app.route("/dashboard.html")
-def about():
   return render_template("dashboard.html")
+
+# @app.route("/dashboard.html")
+# def about():
+#   return render_template("dashboard.html")
     
 app.run(host='0.0.0.0', port=8080)
