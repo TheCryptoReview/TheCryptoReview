52e91063a21748f80f55abca13237f30ebcc40f1
Theo <theomhalpern@gmail.com>
2022-01-01 19:49:57 +0000

    got ride of extra boxes

--- PriceRetriever.py
+++ PriceRetriever.py
@@ -1,38 +1,38 @@
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+# from requests import Request, Session
+# from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
-import json
+# import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-currency = 'USD'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert': currency
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'de88b749-ce4c-420f-bda7-494b636327b7',
-}
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+# currency = 'USD'
+# parameters = {
+#   'start':'1',
+#   'limit':'5000',
+#   'convert': currency
+# }
+# headers = {
+#   'Accepts': 'application/json',
+#   'X-CMC_PRO_API_KEY': 'de88b749-ce4c-420f-bda7-494b636327b7',
+# }
 
-session = Session()
-session.headers.update(headers)
-ranks = []
+# session = Session()
+# session.headers.update(headers)
+# ranks = []
 
-response = session.get(url, params=parameters)
-data = json.loads(response.text)
+# response = session.get(url, params=parameters)
+# data = json.loads(response.text)
 
-def headingGetter(coinName):
-  for i in range(5000):
-    name = data['data'][i]['name']
-    if coinName == name:
-      finalIndex = i
-      break
+# def headingGetter(coinName):
+#   for i in range(5000):
+#     name = data['data'][i]['name']
+#     if coinName == name:
+#       finalIndex = i
+#       break
 
-  specificData = data['data'][finalIndex]['quote'][currency]
+#   specificData = data['data'][finalIndex]['quote'][currency]
 
-  return specificData['price'], specificData['percent_change_1h'], +specificData['percent_change_24h'], \
-        specificData['percent_change_7d'], specificData['percent_change_30d'], specificData['percent_change_60d'], \
-        specificData['percent_change_90d'], specificData['market_cap']
+#   return specificData['price'], specificData['percent_change_1h'], +specificData['percent_change_24h'], \
+#         specificData['percent_change_7d'], specificData['percent_change_30d'], specificData['percent_change_60d'], \
+#         specificData['percent_change_90d'], specificData['market_cap']
 
 
