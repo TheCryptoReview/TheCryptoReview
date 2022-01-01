b77e58f3c2735acddb9ad27ff20887b89858f17e
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2022-01-01 11:04:15 -0800

    Model V2 Almost Complete
    Everything is done except for returning data in the required format.

--- CryptoModelV2.py
+++ CryptoModelV2.py
@@ -1,39 +1,58 @@
+import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 from prophet import Prophet
 import matplotlib.pyplot as plt
 
-# Retrieve Data
-crypto_currency = 'BTC'
-against_currency = 'USD'
+def predictCrypto(ticker):
+    # Retrieve Data
+    crypto_currency = ticker.upper()
+    against_currency = 'USD'
 
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
+    start = dt.datetime(2016, 1, 1)
+    end = dt.datetime.now()
 
-df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
-df['ds'] = df.index
-df['y'] = df['Close']
+    periods = 180
 
-print(df.head(2))
+    df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+    df['ds'] = df.index
+    df['y'] = df['Close']
 
-m = Prophet()
-m.fit(df)
+    # print(df.head(2))
 
-future = m.make_future_dataframe(periods=180)
-future.tail(2)
+    m = Prophet()
+    m.fit(df)
 
-forecast= m.predict(future)
-forecast.tail(2)
+    future = m.make_future_dataframe(periods=180)
+    future.tail(2)
 
-plt.figure(figsize=(15,5))
-plt.title(crypto_currency + against_currency + ' price prediction')
-plt.plot(df['ds'], df['Close'])
-plt.plot(forecast['ds'], forecast['yhat'])
-plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
-plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
-plt.legend('upper right')
+    forecast = m.predict(future)
+    forecast.tail(2)
 
-plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
-#plt.plot(forecast['ds'], forecast['trend'])
+    plt.figure(figsize=(15,5))
+    plt.title(crypto_currency + against_currency + ' price prediction')
+    plt.plot(df['ds'], df['Close'])
+    plt.plot(forecast['ds'], forecast['yhat'], color='green')
+    plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
+    plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
+    plt.legend('upper right')
 
-plt.show()
+    plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
+    plt.plot(forecast['ds'], forecast['trend'])
+
+    plt.show()
+
+    predicted_data = forecast[len(df['Close']):len(forecast['yhat'])]
+    predicted_data.set_index(predicted_data['ds'], drop=True, append=False, inplace=True)
+    predicted_data = predicted_data[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
+
+    # predicted_data.drop(columns=['ds'])
+    print(predicted_data.index)
+    print(predicted_data.columns)
+
+    forecast.set_index(forecast['ds'], drop=True, append=False, inplace=True)
+    forecast = forecast[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
+    forecast.to_csv('FullEthereumData.csv')
+
+
+predictCrypto('DOGE')
