519149a934e6568a73e79125fd4859b97eb8b20f
Theo <theomhalpern@gmail.com>
2022-01-01 17:46:16 +0000

    trying to get flask to run

--- CryptoModelV2.py
+++ CryptoModelV2.py
@@ -1,39 +1,39 @@
-import pandas_datareader as web
-import datetime as dt
-from prophet import Prophet
-import matplotlib.pyplot as plt
+# import pandas_datareader as web
+# import datetime as dt
+# from prophet import Prophet
+# import matplotlib.pyplot as plt
 
-# Retrieve Data
-crypto_currency = 'BTC'
-against_currency = 'USD'
+# # Retrieve Data
+# crypto_currency = 'BTC'
+# against_currency = 'USD'
 
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
+# start = dt.datetime(2016, 1, 1)
+# end = dt.datetime.now()
 
-df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
-df['ds'] = df.index
-df['y'] = df['Close']
+# df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+# df['ds'] = df.index
+# df['y'] = df['Close']
 
-print(df.head(2))
+# print(df.head(2))
 
-m = Prophet()
-m.fit(df)
+# m = Prophet()
+# m.fit(df)
 
-future = m.make_future_dataframe(periods=180)
-future.tail(2)
+# future = m.make_future_dataframe(periods=180)
+# future.tail(2)
 
-forecast= m.predict(future)
-forecast.tail(2)
+# forecast= m.predict(future)
+# forecast.tail(2)
 
-plt.figure(figsize=(15,5))
-plt.title(crypto_currency + against_currency + ' price prediction')
-plt.plot(df['ds'], df['Close'])
-plt.plot(forecast['ds'], forecast['yhat'])
-plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
-plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
-plt.legend('upper right')
+# plt.figure(figsize=(15,5))
+# plt.title(crypto_currency + against_currency + ' price prediction')
+# plt.plot(df['ds'], df['Close'])
+# plt.plot(forecast['ds'], forecast['yhat'])
+# plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
+# plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
+# plt.legend('upper right')
 
-plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
-#plt.plot(forecast['ds'], forecast['trend'])
+# plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
+# #plt.plot(forecast['ds'], forecast['trend'])
 
-plt.show()
+# plt.show()
