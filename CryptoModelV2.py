16d09de3ea0fe47f41747d2f60c033b5df488c0d
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2022-01-01 12:39:17 -0800

    Added CryptoVolatility and Requirements.txt

--- CryptoModelV2.py
+++ CryptoModelV2.py
@@ -1,8 +1,6 @@
-import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 from prophet import Prophet
-import matplotlib.pyplot as plt
 
 def predictCrypto(ticker):
     # Retrieve Data
@@ -18,8 +16,6 @@ def predictCrypto(ticker):
     df['ds'] = df.index
     df['y'] = df['Close']
 
-    # print(df.head(2))
-
     m = Prophet()
     m.fit(df)
 
@@ -29,30 +25,21 @@ def predictCrypto(ticker):
     forecast = m.predict(future)
     forecast.tail(2)
 
-    plt.figure(figsize=(15,5))
-    plt.title(crypto_currency + against_currency + ' price prediction')
-    plt.plot(df['ds'], df['Close'])
-    plt.plot(forecast['ds'], forecast['yhat'], color='green')
-    plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
-    plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
-    plt.legend('upper right')
-
-    plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
-    plt.plot(forecast['ds'], forecast['trend'])
-
-    plt.show()
+    # plt.figure(figsize=(15,5))
+    # plt.title(crypto_currency + against_currency + ' price prediction')
+    # plt.plot(df['ds'], df['Close'])
+    # plt.plot(forecast['ds'], forecast['yhat'], color='green')
+    # plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
+    # plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
+    # plt.legend('upper right')
+    #
+    # plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
+    # plt.plot(forecast['ds'], forecast['trend'])
+    #
+    # plt.show()
 
     predicted_data = forecast[len(df['Close']):len(forecast['yhat'])]
     predicted_data.set_index(predicted_data['ds'], drop=True, append=False, inplace=True)
     predicted_data = predicted_data[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
 
-    # predicted_data.drop(columns=['ds'])
-    print(predicted_data.index)
-    print(predicted_data.columns)
-
-    forecast.set_index(forecast['ds'], drop=True, append=False, inplace=True)
-    forecast = forecast[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
-    forecast.to_csv('FullEthereumData.csv')
-
-
-predictCrypto('DOGE')
+    return predicted_data.to_csv
