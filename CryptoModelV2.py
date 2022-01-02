6f4e8ac0d46f69ff7762bb3f0fef7eefa8026974
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2022-01-01 22:24:20 -0800

    Minor Edits

--- CryptoModelV2.py
+++ CryptoModelV2.py
@@ -12,49 +12,54 @@ def formatTimeDataWithTime(x):
     return x_list
 
 def predictCrypto(ticker, daysToPredict=180):
-    # Retrieve Data
-    crypto_currency = ticker.upper()
-    against_currency = 'USD'
+    try:
+        # Retrieve Data
+        crypto_currency = ticker.upper()
+        against_currency = 'USD'
 
-    start = dt.datetime(2016, 1, 1)
-    end = dt.datetime.now()
+        start = dt.datetime(2016, 1, 1)
+        end = dt.datetime.now()
 
-    periods = daysToPredict
+        periods = daysToPredict
 
-    df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
-    df['ds'] = df.index
-    df['y'] = df['Close']
+        df = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+        df['ds'] = df.index
+        df['y'] = df['Close']
 
-    m = Prophet()
-    m.fit(df)
+        m = Prophet()
+        m.fit(df)
 
-    future = m.make_future_dataframe(periods=180)
-    future.tail(2)
+        future = m.make_future_dataframe(periods=180)
+        future.tail(2)
 
-    forecast = m.predict(future)
-    forecast.tail(2)
+        forecast = m.predict(future)
+        forecast.tail(2)
 
-    # plt.figure(figsize=(15,5))
-    # plt.title(crypto_currency + against_currency + ' price prediction')
-    # plt.plot(df['ds'], df['Close'])
-    # plt.plot(forecast['ds'], forecast['yhat'], color='green')
-    # # plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
-    # # plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
-    # plt.legend('upper right')
-    #
-    # # plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
-    # # plt.plot(forecast['ds'], forecast['trend'])
-    #
-    # plt.show()
+        # plt.figure(figsize=(15,5))
+        # plt.title(crypto_currency + against_currency + ' price prediction')
+        # plt.plot(df['ds'], df['Close'])
+        # plt.plot(forecast['ds'], forecast['yhat'], color='green')
+        # # plt.plot(forecast['ds'], forecast['yhat_lower'], alpha=0.1, color='blue')
+        # # plt.plot(forecast['ds'], forecast['yhat_upper'], alpha=0.1, color='blue')
+        # plt.legend('upper right')
+        #
+        # # plt.fill_between(forecast['ds'], forecast['yhat_upper'], forecast['yhat_lower'], alpha=0.1)
+        # # plt.plot(forecast['ds'], forecast['trend'])
+        #
+        # plt.show()
 
-    predicted_data = forecast[len(df['Close']):len(forecast['yhat'])]
-    predicted_data.set_index(predicted_data['ds'], drop=True, append=False, inplace=True)
-    predicted_data = predicted_data[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
+        predicted_data = forecast[len(df['Close']):len(forecast['yhat'])]
+        predicted_data.set_index(predicted_data['ds'], drop=True, append=False, inplace=True)
+        predicted_data = predicted_data[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
 
-    forecast.set_index(forecast['ds'], drop=True, append=False, inplace=True)
-    forecast = forecast[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
+        forecast.set_index(forecast['ds'], drop=True, append=False, inplace=True)
+        forecast = forecast[['trend', 'yhat_lower', 'yhat_upper', 'yhat']]
 
-    return formatTimeDataWithTime(df.index), df['Close'].to_numpy().tolist(), formatTimeDataWithTime(forecast.index), forecast['yhat'].to_numpy().tolist()
+        return formatTimeDataWithTime(df.index), df['Close'].to_numpy().tolist(), formatTimeDataWithTime(forecast.index), forecast['yhat'].to_numpy().tolist()
+    except:
+        print("Error")
+        return None, None, None, None
+
 
 x_real, y_real, x_predicted, y_predicted = predictCrypto("IOTX", 180)
 print(x_real)
