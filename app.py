10cd218751bba47473b7b36e147d91c8cd76a71a
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2022-01-03 00:19:55 -0800

    Reformat app.py for Routing

--- app.py
+++ app.py
@@ -19,136 +19,57 @@ def index():
 def dashboard():
     if request.method == 'POST':
         coinName = request.form['coinName']
-        name, symbol, price, pchange_1h, pchange_24h, pchange_7d, pchange_30d, pchange_60d, pchange_90d, market_cap = getCryptoData(
-            coinName)
-        x_real, y_real, x_predicted, y_predicted = predictCrypto(symbol, daysToPredict=180)
-
-        price = roundCrypto(price)
-        pchange_1h = roundCrypto(pchange_1h)
-        pchange_24h = roundCryptoWithoutDollar(pchange_24h)
-        pchange_7d = roundCryptoWithoutDollar(pchange_7d)
-        pchange_30d = roundCryptoWithoutDollar(pchange_30d)
-        pchange_60d = roundCryptoWithoutDollar(pchange_60d)
-        pchange_90d = roundCryptoWithoutDollar(pchange_90d)
-        market_cap = roundCrypto(market_cap)
-
-        data = {
-            'coin_name': name,
-            'coin_ticker': symbol,
-            'dates': x_predicted,
-            'historical_data': y_real,
-            'predicted_data': y_predicted,
-            'price': price,
-            'pchange_24h': pchange_24h,
-            'pchange_7d': pchange_7d,
-            'pchange_30d': pchange_30d,
-            'pchange_60d': pchange_60d,
-            'pchange_90d': pchange_90d,
-            'market_cap': market_cap
-        }
-
-        confidence = 0
-        preLast = y_predicted[len(y_predicted) - 1]
-        realLast = y_real[len(y_real) - 1]
-        if preLast <= realLast:
-            confidence = (1 - ((realLast - preLast) / realLast)) * 50
-        else:
-            confidence = (1 - ((realLast - preLast) / preLast)) * 50
-        confidence = round(confidence, 1)
-        fearRating, emotion = priceGreed()
-
-        print(confidence, fearRating)
-
-        return render_template("dashboardBigGraph.html", data=data, name=name, cr=confidence, fg=fearRating)
+        return redirect("/dashboard.html?token="+coinName, code=302)
 
     try:
         coinName = request.args.get("token")
-        name, symbol, price, pchange_1h, pchange_24h, pchange_7d, pchange_30d, pchange_60d, pchange_90d, market_cap = getCryptoData(
-            coinName)
-        x_real, y_real, x_predicted, y_predicted = predictCrypto(symbol, daysToPredict=180)
+        try:
+            name, symbol, price, pchange_1h, pchange_24h, pchange_7d, pchange_30d, pchange_60d, pchange_90d, market_cap = getCryptoData(
+                coinName)
+            x_real, y_real, x_predicted, y_predicted = predictCrypto(symbol, daysToPredict=180)
 
-        price = roundCrypto(price)
-        pchange_1h = roundCrypto(pchange_1h)
-        pchange_24h = roundCryptoWithoutDollar(pchange_24h)
-        pchange_7d = roundCryptoWithoutDollar(pchange_7d)
-        pchange_30d = roundCryptoWithoutDollar(pchange_30d)
-        pchange_60d = roundCryptoWithoutDollar(pchange_60d)
-        pchange_90d = roundCryptoWithoutDollar(pchange_90d)
-        market_cap = roundCrypto(market_cap)
+            price = roundCrypto(price)
+            pchange_1h = roundCrypto(pchange_1h)
+            pchange_24h = roundCryptoWithoutDollar(pchange_24h)
+            pchange_7d = roundCryptoWithoutDollar(pchange_7d)
+            pchange_30d = roundCryptoWithoutDollar(pchange_30d)
+            pchange_60d = roundCryptoWithoutDollar(pchange_60d)
+            pchange_90d = roundCryptoWithoutDollar(pchange_90d)
+            market_cap = roundCrypto(market_cap)
 
-        data = {
-            'coin_name': name,
-            'coin_ticker': symbol,
-            'dates': x_predicted,
-            'historical_data': y_real,
-            'predicted_data': y_predicted,
-            'price': price,
-            'pchange_24h': pchange_24h,
-            'pchange_7d': pchange_7d,
-            'pchange_30d': pchange_30d,
-            'pchange_60d': pchange_60d,
-            'pchange_90d': pchange_90d,
-            'market_cap': market_cap
-        }
+            data = {
+                'coin_name': name,
+                'coin_ticker': symbol,
+                'dates': x_predicted,
+                'historical_data': y_real,
+                'predicted_data': y_predicted,
+                'price': price,
+                'pchange_24h': pchange_24h,
+                'pchange_7d': pchange_7d,
+                'pchange_30d': pchange_30d,
+                'pchange_60d': pchange_60d,
+                'pchange_90d': pchange_90d,
+                'market_cap': market_cap
+            }
 
-        confidence = 0
-        preLast = y_predicted[len(y_predicted) - 1]
-        realLast = y_real[len(y_real) - 1]
-        if preLast <= realLast:
-            confidence = (1 - ((realLast - preLast) / realLast)) * 50
-        else:
-            confidence = (1 - ((realLast - preLast) / preLast)) * 50
-        confidence = round(confidence, 1)
-        fearRating, emotion = priceGreed()
+            confidence = 0
+            preLast = y_predicted[len(y_predicted) - 1]
+            realLast = y_real[len(y_real) - 1]
+            if preLast <= realLast:
+                confidence = (1 - ((realLast - preLast) / realLast)) * 50
+            else:
+                confidence = (1 - ((realLast - preLast) / preLast)) * 50
+            confidence = round(confidence, 1)
+            fearRating, emotion = priceGreed()
 
-        print(confidence, fearRating)
-
-        return render_template("dashboardBigGraph.html", data=data, name=name, cr=confidence, fg=fearRating)
+            return render_template("dashboardBigGraph.html", data=data, name=name,fg=fearRating,cr=confidence)
+        except:
+            print("Error")
     except:
         print("No token")
 
     coinName = 'BTC'
-    name, symbol, price, pchange_1h, pchange_24h, pchange_7d, pchange_30d, pchange_60d, pchange_90d, market_cap = getCryptoData(
-        coinName)
-    x_real, y_real, x_predicted, y_predicted = predictCrypto(symbol, daysToPredict=180)
-
-    price = roundCrypto(price)
-    pchange_1h = roundCrypto(pchange_1h)
-    pchange_24h = roundCryptoWithoutDollar(pchange_24h)
-    pchange_7d = roundCryptoWithoutDollar(pchange_7d)
-    pchange_30d = roundCryptoWithoutDollar(pchange_30d)
-    pchange_60d = roundCryptoWithoutDollar(pchange_60d)
-    pchange_90d = roundCryptoWithoutDollar(pchange_90d)
-    market_cap = roundCrypto(market_cap)
-
-    data = {
-        'coin_name': name,
-        'coin_ticker': symbol,
-        'dates': x_predicted,
-        'historical_data': y_real,
-        'predicted_data': y_predicted,
-        'price': price,
-        'pchange_24h': pchange_24h,
-        'pchange_7d': pchange_7d,
-        'pchange_30d': pchange_30d,
-        'pchange_60d': pchange_60d,
-        'pchange_90d': pchange_90d,
-        'market_cap': market_cap
-    }
-
-    confidence = 0
-    preLast = y_predicted[len(y_predicted) - 1]
-    realLast = y_real[len(y_real) - 1]
-    if preLast <= realLast:
-        confidence = (1 - ((realLast - preLast) / realLast)) * 50
-    else:
-        confidence = (1 - ((realLast - preLast) / preLast)) * 50
-    confidence = round(confidence, 1)
-    fearRating, emotion = priceGreed()
-
-    print(confidence, fearRating)
-
-    return render_template("dashboardBigGraph.html", data=data, name=name, cr=confidence, fg=fearRating)
+    return redirect("/dashboard.html?token=" + coinName, code=302)
 
 @app.route("/about.html")
 def about():
@@ -160,4 +81,5 @@ def education():
 
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0', port=8080, debug=True)
+  # app.run()
