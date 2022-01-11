1f68ba2c347208ae500d58e8c3ff2e6539ceae5d
Abhijith Varma Mudunuri <66578240+mabhi02@users.noreply.github.com>
2022-01-10 17:28:42 -0800

    Update PriceRetriever.py

--- PriceRetriever.py
+++ PriceRetriever.py
@@ -13,7 +13,7 @@ parameters = {
 global headers
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'b42a88f4-4a06-4f10-a665-12b9af4f8705',
+  'X-CMC_PRO_API_KEY': '15518944-2395-4c5c-8134-8aca38572fb1',
 }
 
 def headingGetter(coinName):
